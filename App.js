68dd35551b76745bf15e7f6bfa66821a2cd2b5d2
Bryce77 <71152555+Bryce77@users.noreply.github.com>
2021-01-27 14:02:59 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <AppContainer />
+    </View>
+  );
+}
+
+var AppNavigator = createSwitchNavigator({
+  BookScreen: BookScreen,
+  SearchScreen: SearchScreen,
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
